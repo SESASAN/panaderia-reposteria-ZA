6f800643ae5d1d3163106cfa0cd85e274ef4ba4e
Sebastián <sebastianjesus.perez@gmail.com>
2025-04-11 23:23:34 -0500

    Feature: Añade nueva vista Home y layout base

---
+++ resources/views/components/Header.blade.php
@@ -0,0 +1,24 @@
+{{-- El text-lg representa un texto en 18px --}}
+<header class="font-nunito text-lg">
+    {{-- h-24 significa 96pixeles de altura y px-4 es el padding en el eje x de 16px --}}
+    <nav class="flex justify-between items-center h-24 px-4">
+        <div class="">
+            <img class="w-30" src="{{asset('images/Logo2.jpg')}}" alt="">
+        </div>
+        <div class="bg-white h-full flex items-center">
+            <ul class="flex items-center gap-2 px-4">
+                <li>
+                    <a class="hover:text-gray-500 px-10 mx-5 border-x-1 border-black" href="#">Repostería</a>
+                </li>
+                <li>
+                    <a class="hover:text-gray-500 px-10 mx-5 border-x-1 border-black" href="#">Panadería</a>
+                </li>
+            </ul>
+        </div>
+        <div>
+            {{-- <button class="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">
+                Iniciar Sesión
+            </button> --}}
+        </div>
+    </nav>
+</header>
