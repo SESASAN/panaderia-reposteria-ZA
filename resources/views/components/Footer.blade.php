e099e5a19142e2f74e6d9d7b402a0332714ebfb9
NathyGaby04 <nathalie040904@gmail.com>
2025-04-18 20:46:34 -0500

    Updated footer

--- resources/views/components/Footer.blade.php
+++ resources/views/components/Footer.blade.php
@@ -1,5 +1,106 @@
-<footer>
-
-
-
-</footer>
+<footer class="py-12 lg:py-5 border-t border-neutral-100">
+      <div class="w-[96%] max-w-7xl mx-auto">
+        <div
+          class="border-b border-neutral-100 pb-8 lg:pb-5 flex justify-between flex-col lg:flex-row items-center lg:items-start"
+        >
+          <div
+            class="space-y-5 pb-4 border-b border-neutral-100 lg:pb-1 lg:border-none w-full flex flex-col lg:block items-center"
+          >
+            <img src="{{asset('images/Logo2.jpg')}}" alt="Panadería y repostería Zhandra Araujo" class="w-40" />
+            <ul
+              class="flex gap-x-8 text-xs text-gray-500 flex-col lg:flex-row gap-y-6 text-center lg:text-start"
+            >
+              <li>
+                <a href="#" class="hover:text-red-600">Productos y Servicios</a>
+              </li>
+              <li><a href="#" class="hover:text-red-600">Contacto</a></li>
+              <li><a href="#" class="hover:text-red-600">Sobre nosotros</a></li>
+            </ul>
+            <!-- Social icons -->
+            <div class="flex items-center gap-x-4 text-gray-700">
+              <!-- Tiktok icon -->
+              <a href="#" class="hover:text-red-600 cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="24"
+                  height="24"
+                  viewBox="00 0 448 512"
+                  fill="currentColor"
+                >
+                  <path
+                    d="M448 209.9a210.1 210.1 0 0 1 -122.8-39.3V349.4A162.6 162.6 0 1 1 185 188.3V278.2a74.6 74.6 0 1 0 52.2 71.2V0l88 0a121.2 121.2 0 0 0 1.9 22.2h0A122.2 122.2 0 0 0 381 102.4a121.4 121.4 0 0 0 67 20.1z"
+                  ></path>
+                </svg>
+              </a>
+              <!-- Instagram icon -->
+              <a href="#" class="hover:text-red-600 cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 32 32"
+                  fill="currentColor"
+                >
+                  <path
+                    d="M 11.46875 5 C 7.917969 5 5 7.914063 5 11.46875 L 5 20.53125 C 5 24.082031 7.914063 27 11.46875 27 L 20.53125 27 C 24.082031 27 27 24.085938 27 20.53125 L 27 11.46875 C 27 7.917969 24.085938 5 20.53125 5 Z M 11.46875 7 L 20.53125 7 C 23.003906 7 25 8.996094 25 11.46875 L 25 20.53125 C 25 23.003906 23.003906 25 20.53125 25 L 11.46875 25 C 8.996094 25 7 23.003906 7 20.53125 L 7 11.46875 C 7 8.996094 8.996094 7 11.46875 7 Z M 21.90625 9.1875 C 21.402344 9.1875 21 9.589844 21 10.09375 C 21 10.597656 21.402344 11 21.90625 11 C 22.410156 11 22.8125 10.597656 22.8125 10.09375 C 22.8125 9.589844 22.410156 9.1875 21.90625 9.1875 Z M 16 10 C 12.699219 10 10 12.699219 10 16 C 10 19.300781 12.699219 22 16 22 C 19.300781 22 22 19.300781 22 16 C 22 12.699219 19.300781 10 16 10 Z M 16 12 C 18.222656 12 20 13.777344 20 16 C 20 18.222656 18.222656 20 16 20 C 13.777344 20 12 18.222656 12 16 C 12 13.777344 13.777344 12 16 12 Z"
+                  ></path>
+                </svg>
+              </a>
+              <!-- LinkedIn icon -->
+              <a href="#" class="hover:text-red-600 cursor-pointer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  x="0px"
+                  y="0px"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 30 30"
+                  fill="currentColor"
+                >
+                  <path
+                    d="M24,4H6C4.895,4,4,4.895,4,6v18c0,1.105,0.895,2,2,2h18c1.105,0,2-0.895,2-2V6C26,4.895,25.105,4,24,4z M10.954,22h-2.95 v-9.492h2.95V22z M9.449,11.151c-0.951,0-1.72-0.771-1.72-1.72c0-0.949,0.77-1.719,1.72-1.719c0.948,0,1.719,0.771,1.719,1.719 C11.168,10.38,10.397,11.151,9.449,11.151z M22.004,22h-2.948v-4.616c0-1.101-0.02-2.517-1.533-2.517 c-1.535,0-1.771,1.199-1.771,2.437V22h-2.948v-9.492h2.83v1.297h0.04c0.394-0.746,1.356-1.533,2.791-1.533 c2.987,0,3.539,1.966,3.539,4.522V22z"
+                  ></path>
+                </svg>
+              </a>
+            </div>
+            <!-- Social icons -->
+          </div>
+          <!-- <div class="pt-8 lg:pt-0 max-w-[624px] w-full">
+            <h4
+              class="mb-2 font-medium text-lg leading-7 text-gray-900 text-center lg:text-start"
+            >
+              ¿Quieres alguna información?
+            </h4>
+            <p class="mb-6 font-medium text-gray-500 text-center lg:text-start">
+              Si deseas trabajar con nosotros o quieres alguna información ingresa tu email y te contactaremos.
+            </p>
+            <form class="flex gap-x-3 w-full">
+              <input
+                type="email"
+                placeholder="Email"
+                class="border rounded-lg p-3 text-sm focus:outline-red-600 flex-1"
+              />
+              <input
+                type="submit"
+                value="Enviar"
+                class="bg-red-600 rounded-lg py-3 px-5 text-white text-sm cursor-pointer hover:brightness-95"
+              />
+            </form>
+          </div> -->
+        </div>
+        <div class="pt-8 flex justify-between flex-col lg:flex-row gap-y-4 items-center">
+          <ul class="flex gap-x-8 text-xs text-gray-500">
+            <li><a href="#" class="hover:text-red-600">Español</a></li>
+            <li><a href="#" class="hover:text-red-600">Privacy</a></li>
+            <li><a href="#" class="hover:text-red-600">Legal</a></li>
+          </ul>
+          <p class="text-xs text-gray-400">
+            © 2025 Panadería y repostería Zhandra Araujo.
+          </p>
+        </div>
+      </div>
+    </footer>
